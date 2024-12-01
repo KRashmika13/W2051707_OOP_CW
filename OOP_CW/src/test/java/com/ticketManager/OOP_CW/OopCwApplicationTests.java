69c53a5a184f2b42544e87139bb4ac48ab54c247
KRashmika13 <krashmika13@gmail.com>
2024-12-01 14:51:53 +0530

    update with spring boot

---
+++ OOP_CW/src/test/java/com/ticketManager/OOP_CW/OopCwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketManager.OOP_CW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
