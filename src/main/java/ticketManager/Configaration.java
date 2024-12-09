fb8ffed16d241b23ec89ac7c9ebded66c7592643
KRashmika13 <krashmika13@gmail.com>
2024-12-09 21:22:14 +0530

    Create Configuration class

--- src/main/java/ticketManager/Configaration.java
+++ src/main/java/ticketManager/Configaration.java
@@ -1,14 +1,18 @@
 package ticketManager;
 
-import java.io.Serializable;
+import com.google.gson.Gson;
 
-public class Configaration implements Serializable {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Configuration {
     private int totalTickets;
     private int ticketReleaseRate;
     private int customerRetrievalRate;
     private int maxTicketCapacity;
 
-    public Configaration(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity) {
+    public Configuration(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity) {
         this.totalTickets = totalTickets;
         this.ticketReleaseRate = ticketReleaseRate;
         this.customerRetrievalRate = customerRetrievalRate;
@@ -30,5 +34,24 @@ public class Configaration implements Serializable {
     public int getMaxTicketCapacity() {
         return maxTicketCapacity;
     }
-}
 
+    public void saveToFile(String filename) {
+        try(FileWriter writer = new FileWriter(filename)){
+            Gson gson = new Gson();
+            gson.toJson(this, writer);
+            System.out.println("Configuration saved to " + filename);
+        }catch(IOException e){
+            System.out.println("Failed to save configuration: " + e.getMessage());
+        }
+    }
+
+    public static Configuration loadFromFile(String filename) {
+        try(FileReader reader = new FileReader(filename)){
+            Gson gson = new Gson();
+            return gson.fromJson(reader, Configuration.class);
+        }catch (IOException e){
+            System.err.println("Failed to load configuration: " + e.getMessage());
+            return null;
+        }
+    }
+}
